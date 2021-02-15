a1bb01de4673e926307ee1470b0f043e2bc98bf6
gtbashish <ashishahead1@gmail.com>
2021-02-15 16:35:36 +0530

    Contracts of Data sources

---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:tdd_flutter/core/error/failures.dart';
+import 'package:tdd_flutter/core/platform/network_info.dart';
+import 'package:tdd_flutter/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:tdd_flutter/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:tdd_flutter/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:tdd_flutter/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl(
+      {@required this.remoteDataSource,
+      @required this.localDataSource,
+      @required this.networkInfo});
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
+    return null;
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    return null;
+  }
+}
